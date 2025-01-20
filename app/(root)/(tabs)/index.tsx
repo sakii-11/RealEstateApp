3f39ce9070e08aa930659d62041700165c61b985
Your New Username <149531974+sakii-11@users.noreply.github.com>
2025-01-20 22:21:56 +0530

    initial project setup

---
+++ app/(root)/(tabs)/index.tsx
@@ -0,0 +1,18 @@
+import { View, Text } from 'react-native'
+import React from 'react';
+import { Link } from 'expo-router';
+
+export default function Index() {
+  return (
+    <View
+     style = {{
+      flex:1,
+      justifyContent : "center",
+      alignItems : "center"
+     }}
+    >
+      <Text className='font-bold my-10 font-rubik text-3xl'>Welcome to RealEstate App </Text>
+      
+    </View>
+  );
+}
---
+++ app/(root)/(tabs)/profile.tsx
@@ -0,0 +1,64 @@
+import { View, Text, ScrollView, Image, TouchableOpacity, ImageSourcePropType } from 'react-native'
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context';
+import icons from '@/constants/icons';
+import images from '@/constants/images';
+import { settings } from '@/constants/data';
+
+interface SettingsItemProps {
+  icon: ImageSourcePropType;
+  title : string;
+  onPress?: () => void;
+  textStyle?: any ;
+  showArrow?: boolean;
+}
+
+const SettingsItem = ({icon, title, onPress, textStyle, showArrow = true} : SettingsItemProps) => (
+  <TouchableOpacity className='flex flex-row items-center justify-between py-3'>
+    <View className='flex flex-row items-center gap-3'>
+      <Image source={icon} className='size-6' />
+      <Text className={`text-lg font-rubik-medium text-black-300 ${textStyle}`}>{title}</Text>
+    </View>
+    {showArrow && <Image source={icons.rightArrow} className='size-5' />}
+  </TouchableOpacity>
+)
+
+const profile = () => {
+  const handleLogout = async () => {};
+
+  return (
+    <SafeAreaView>
+      <ScrollView
+      showsVerticalScrollIndicator={false}
+      contentContainerClassName='pb-32 px-7'
+      >
+      <View className='flex flex-row items-center justify-between mt-5'>
+          <Text className='text-xl font-rubik-bold'>Profile</Text>
+          <Image source={icons.bell} className="size-5" />
+      </View>
+      <View className='flex-row justify-center flex mt-5'>
+        <View className='flex flex-col items-center relative mt-5'>
+          <Image source={images.avatar} className='size-44 relative rounded-full' />
+          <TouchableOpacity className='absolute bottom-11 right-2'>
+            <Image source={icons.edit} className='size-9'/>
+          </TouchableOpacity>
+          <Text className='text-2xl font-rubik-bold mt-2'> Sakii </Text>
+        </View>
+      </View>
+      <View className='flex flex-col mt-10'>
+          <SettingsItem icon={icons.calendar} title='My Bookings' />
+          <SettingsItem icon={icons.wallet} title='Payments' />
+      </View>
+
+      <View className='flex flex-col mt-5 border-t pt-5 border-primary-200'>
+        {settings.slice(2).map((item, index) => (
+          <SettingsItem key={index} {... item} />
+        ))}
+
+      </View>
+      </ScrollView>
+    </SafeAreaView>
+  )
+}
+
+export default profile
